9c8bb207757044418faba08eeb439d6647a2f9ab
KaterinaOrlova <79505124+KaterinaOrlova@users.noreply.github.com>
2021-02-25 10:29:01 +0200

    Update 10b_2gr.py
    Pierakstiju formulu 4 rindā.

--- 10b_2gr.py
+++ 10b_2gr.py
@@ -4,23 +4,23 @@ Kudrjavceva Veronika
 Melnic Ana
 3 	
 Mihailovs Alekss
-4 	
-Orlova Katerina
+print(9-2)	
+#Orlova Katerina
 5 	
-Rimare Sabîne
+Rimare SabÃ®ne
 6 	
 Sidorenko Aleksandrs
 7 	
-Siïiòğ Deniss
+SiÃ¯iÃ²Ã° Deniss
 8 	
 Skodorovs Vlads
 9 	
 Smirnovs Volodars
 10 	
-Verşanovskis Marks
+VerÃ¾anovskis Marks
 11 	
 Volkova Veronika
 12 	
-Zariòğ Roberts
+ZariÃ²Ã° Roberts
 13 	
-Şaravins Kirills
+Ãaravins Kirills
